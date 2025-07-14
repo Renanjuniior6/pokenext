6a87c08a513273aa243ebfcc25cdfcc93b5f6a88
Renanjuniior6 <renanjrr679@gmail.com>
2025-07-14 15:26:55 -0300

    feat: renderizacao dos pokemons

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -9,7 +9,6 @@ export default function About() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae neque massa. Morbi ac iaculis nibh, eu scelerisque risus. Nam ac velit pharetra, euismod lorem et, interdum nulla. Sed placerat, leo sed sollicitudin aliquet, leo diam laoreet justo, nec bibendum nisl tortor eu augue.
         </p>
         <Image src="/images/charizard.png" width={300} height={300} alt="charizard"/>
-
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,32 @@
-export default function Home() {
+async function getStaticProps() {
+
+  const maxPokemons = 251
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`)
+  const data = await res.json()
+
+  // implementando index em cada pokemon
+
+  data.results.forEach((item, index) => {
+    item.id = index + 1
+  });
+
+  return data.results
+}
+
+export default async function Home() {
+
+  const pokemons = await getStaticProps();
+
   return (
     <div>
         <h1>PokeNext</h1>
+        <ul>
+          {pokemons.map((pokemon) => (
+            <li key={pokemon.id}>{pokemon.name}</li>
+          ))}
+        </ul>
     </div>
   );
 }
