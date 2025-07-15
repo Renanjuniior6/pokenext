aa9433c4a1d7d2dae244a043607282203c263d22
Renanjuniior6 <renanjrr679@gmail.com>
2025-07-15 16:43:34 -0300

    style: finalizacao da estilizacao de card

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -1,14 +1,18 @@
 import Image from "next/image"
+import Link from "next/link"
+import styles from "../../styles/Card.module.css"
 
 export default function Card({ pokemon }) {
     return (
-        <div>
+        <div className={styles.card}>
             <Image src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
             width={120}
             height={120}
             alt={pokemon.name}
             />
-            <h3>{pokemon.name}</h3>
+            <p className={styles.id}>#{pokemon.id}</p>
+            <h3 className={styles.title}>{pokemon.name}</h3>
+            <Link href={`/pokemon/${pokemon.id}`} className={styles.btn}>Detalhes</Link>
         </div>
     )
 }
